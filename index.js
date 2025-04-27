2c1c86955afad0cb1eec6fe9614cb307f039ff48
Tazidi <rizki.tazidi@gmail.com>
2025-04-27 19:38:52 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mysql = require('mysql2');
+const app = express();
+
+// Koneksi ke database RDS
+const db = mysql.createConnection({
+  host: 'dbuts.c7qqsi6cyx23.ap-southeast-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'Kepomanehmah1',
+  database: 'ecommerce'
+});
+
+// Endpoint untuk ambil hanya kolom price
+app.get('/products', (req, res) => {
+  db.query('SELECT price FROM products', (err, results) => {
+    if (err) {
+      console.error('Gagal ambil data produk:', err);
+      return res.status(500).send('Gagal ambil produk');
+    }
+    res.json(results);
+  });
+});
+
+app.use(express.static('views'));
+
+app.listen(3000, () => {
+  console.log('Server berjalan di http://localhost:3000');
+});
